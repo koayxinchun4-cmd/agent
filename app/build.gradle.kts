cfe7ddfe6cadbafa9b6638c004d1967b665a2dbe
cc <275708539+koayxinchun4-cmd@users.noreply.github.com>
2026-09-01 16:31:58 +0800

    update buil.gradle.kts add germini sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,6 +101,7 @@ dependencies {
   implementation(libs.coil.compose)
   implementation(libs.converter.moshi)
   implementation(libs.firebase.ai)
+  implementation("com.google.ai.client.generativeai:generativeai:0.9.0")
   // Uncomment to use Firestore:
   // implementation(libs.firebase.firestore)
 
